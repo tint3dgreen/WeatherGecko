4defc74415c1cdc2db192e6be8ff44cb7fffaa21
Colin Arscott <carscott@optonline.net>
2019-11-03 03:18:06 -0500

    Added Gecko bean

---
+++ GeicoWebApp/src/java/Geico/GeckoBean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Geico;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+/**
+ *
+ * @author carsc
+ */
+@ManagedBean(name = "geckoBean")
+@SessionScoped
+public class GeckoBean {
+    
+}
